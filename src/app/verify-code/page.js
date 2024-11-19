964a6007c16f522b5d4778220d743a43cd14fe27
roel <ash49901@gmail.com>
2024-11-19 21:17:48 +0900

    ADD : 다크모드 제거 및 /verify-code 디자인 추가

--- src/app/verify-code/page.js
+++ src/app/verify-code/page.js
@@ -1,4 +1,5 @@
 'use client';
+import {styles} from './useStyle';
 
 export default function VerifyCode() {
 
---
+++ src/app/verify-code/useStyle.js
@@ -0,0 +1,28 @@
+export const styles = {
+    mainText: {
+        fontSize: '24px',
+        fontWeight: '600',
+        color: 'black',
+        lineHeight: '1.5',
+    },
+    subText: {
+        fontSize: '14px',
+        fontWeight: '600',
+        color: '#949494',
+        lineHeight: '1.4',
+    },
+    failText: {
+        color: '#FD6A6A',
+        fontWeight: '600',
+        fontSize: '14px',
+    },
+    marginTopMain: {
+        marginTop: '110px',
+    },
+    marginTopSub: {
+        marginTop: '8px',
+    },
+    marginTopPinCode: {
+        marginTop: '40px',
+    }
+};
