43e3ddc3a10b418f86358892ade5d1909328cbbf
roel <ash49901@gmail.com>
2024-11-21 03:39:38 +0900

    Hotfix : vercel 오류 수정중...(test)

--- src/app/verify-code/page.js
+++ src/app/verify-code/page.js
@@ -9,8 +9,8 @@ import {useTicketValidateMutation} from "../api/mutations";
 export default function VerifyCode(props) {
     const correctCodeText = '사장님의 식권 사용 코드를 입력해 주세요.'
     const incorrectCodeText = '잘못된 코드입니다. 다시 입력해 주세요.'
-    const ticket_id = props.searchParams.ticket_id;
-    console.log(ticket_id)
+    let ticket_id = props.searchParams.ticket_id;
+    ticket_id = ticket_id.replaceAll(' ', '+');
     const [subText, setSubText] = useState(correctCodeText);
     const [isValid, setIsValid] = useState(true);
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -19,8 +19,6 @@ export default function VerifyCode(props) {
 
     const handleTicketValidateSuccess = (data) => {
         // 티켓 유효성 검사 성공 후 처리할 로직
-        console.log("네트워크 연결까진 성공")
-        console.log(data)
         /*{
           "success": true, // true or false
             "data" :
@@ -30,7 +28,7 @@ export default function VerifyCode(props) {
           },
           "error": null
         }*/
-        if(data.success) {
+        if(!data.success) {
             /*올바르지 않은 사용 코드 입력*/
             setIsValid(false);
             setSubText(incorrectCodeText);
@@ -68,22 +66,24 @@ export default function VerifyCode(props) {
         handleTicketValidateError
     )
     const handleCodeComplete = (code) => {
-        // ticketValidateMutation({
-        //   hashed_ticket_id : ticket_id,
-        //   password : code
-        // })
-        if (code === '111111') {
-            handleNetworkSuccess({ title: '인증 성공', message: '인증이 성공적으로 완료되었습니다.', icon: '/icon/ic_check.svg' })
-        } else if (code === '222222') {
-            handleNetworkSuccess({ title: '인증 실패', message: '유효기간이 만료된 쿠폰입니다.', icon: '/icon/ic_warning.svg' })
-        } else if (code === '333333') {
-            handleNetworkSuccess({ title: '인증 실패', message: '이미 사용된 쿠폰입니다.', icon: '/icon/ic_warning.svg' })
-        } else {
-            setIsValid(false);
-            setSubText(incorrectCodeText)
-            setShake(true);
-            setTimeout(() => setShake(false), 500);
-        }
+        console.log(`ticket_id: ${ticket_id}`)
+        console.log(`code: ${code}`)
+        ticketValidateMutation({
+          hashed_ticket_id : ticket_id,
+          password : code
+        })
+        // if (code === '111111') {
+        //     handleNetworkSuccess({ title: '인증 성공', message: '인증이 성공적으로 완료되었습니다.', icon: '/icon/ic_check.svg' })
+        // } else if (code === '222222') {
+        //     handleNetworkSuccess({ title: '인증 실패', message: '유효기간이 만료된 쿠폰입니다.', icon: '/icon/ic_warning.svg' })
+        // } else if (code === '333333') {
+        //     handleNetworkSuccess({ title: '인증 실패', message: '이미 사용된 쿠폰입니다.', icon: '/icon/ic_warning.svg' })
+        // } else {
+        //     setIsValid(false);
+        //     setSubText(incorrectCodeText)
+        //     setShake(true);
+        //     setTimeout(() => setShake(false), 500);
+        // }
     };
 
     const handleConfirm = () => {
