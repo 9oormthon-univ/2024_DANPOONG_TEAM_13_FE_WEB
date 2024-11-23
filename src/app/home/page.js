5e5bc7a85403454840f43c179893710958a0d442
EunJiJung <bianbbc87@gmail.com>
2024-11-24 04:49:21 +0900

    refactor : pt 추가
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -5,10 +5,10 @@ import {styles} from '../component/Modal/useStyle';
 export default function Home() {
   return (
     <div 
-    className="flex justify-center items-center overflow-hidden"
+    className="flex justify-center items-center overflow-hidden pb-20"
       style={{ height: "100vh" }}
     >
-            <main className="flex flex-col row-start-2 items-center">
+            <main className="flex flex-col items-center">
           <div style={{ ...styles.iconContainer, ...styles.b_10px }}> 
             <img src="/icon/ic_check.svg" alt="icon" style={styles.success_icon} />
           </div>
